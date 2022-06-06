830379d75e01e57dafe7d5cf6fb446af411da68a
unknown <edvinaskundrotas@gmail.com>
2022-06-06 14:49:07 +0300

    not working signup

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,152 +1,135 @@
-import React, { useState, useRef } from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
+import React, { useRef, useState } from 'react'
+import { useForm } from "react-hook-form";
 import AuthService from "../services/auth.service";
-const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
-};
-const validEmail = (value) => {
-  if (!isEmail(value)) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This is not a valid email.
-      </div>
-    );
-  }
-};
-const vusername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-const vpassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
-};
-const Register = () => {
-  const form = useRef();
-  const checkBtn = useRef();
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [successful, setSuccessful] = useState(false);
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
-  };
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
-  const handleRegister = (e) => {
-    e.preventDefault();
-    setMessage("");
-    setSuccessful(false);
-    form.current.validateAll();
-    if (checkBtn.current.context._errors.length === 0) {
-      AuthService.register(username, email, password).then(
-        (response) => {
-          setMessage(response.data.message);
-          setSuccessful(true);
-        },
-        (error) => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString();
-          setMessage(resMessage);
-          setSuccessful(false);
-        }
-      );
+import { useNavigate } from 'react-router-dom';
+
+export default function Register() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
+    const navigate = useNavigate();
+    const [usedLogin, setUsedLogin] = useState("")
+    const onSubmit = data => {
+        AuthService.register(data)
+            .then(() => successMessage())
+            .then(() => navigate("/login"))
+            .catch(error => {
+                setUsedLogin(error.response.data.message.length)
+                console.log(error.response.data.message.length)
+                // if (error.response.data.message.length === 34) {
+                //     console.log("something")
+                //     console.log("something: " + usedLogin)
+                // }
+            })
     }
-  };
-  return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="profile-img"
-          className="profile-img-card"
-        />
-        <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={username}
-                  onChange={onChangeUsername}
-                  validations={[required, vusername]}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
-            </div>
-          )}
-          {message && (
-            <div className="form-group">
-              <div
-                className={ successful ? "alert alert-success" : "alert alert-danger" }
-                role="alert"
-              >
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
-      </div>
-    </div>
-  );
-};
-export default Register;
+
+    // Popup message configuration
+    const successMessage = () => {
+        console.log('Registracija sėkminga! Galite prisijungti prie savo paskyros')
+    }
+
+    const password = useRef({});
+    password.current = watch("password", "");
+
+    return (
+        <section className="vh-100">
+            <div className="container h-100">
+                <div className="row d-flex justify-content-center align-items-center h-5">
+                    <div className="col-lg-12 col-xl-11">
+                        <div className="card text-black" style={{ borderRadius: "25px", marginTop: "1em" }}>
+                            <div className="card-body p-md-5">
+                                <div className="row justify-content-center">
+                                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                                        <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registracija
+                                        </p>
+
+                                        <form onSubmit={handleSubmit(onSubmit)} className="mx-1 mx-md-4">
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <label
+                                                        className="form-label"
+                                                        htmlFor="form3Example1c">
+                                                        Jūsų vardas
+                                                    </label>
+                                                    <input
+                                                        {...register("username", { required: true, minLength: 2 })}
+                                                        className="form-control"
+                                                    />
+                                                    {errors?.username?.type === "required" && <p>Vardas yra būtinas</p>}
+                                                    {errors?.username?.type === "minLength" && <p>Vardas turi būti bent 2 simbolių ilgio</p>}
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <label className='mb-2'>El. paštas</label>
+                                                    <input
+                                                        {...register("email",
+                                                            {
+                                                                required: true,
+                                                                pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                                                            })}
+                                                        className="form-control"
+                                                    />
+                                                    {errors?.email?.type === "required" && <p>El. paštas būtinas</p>}
+                                                    {errors?.email?.type === "pattern" && <p>El. paštas turi būti galiojantis</p>}
+                                                    {usedLogin === 31 && <p>El. paštas jau yra naudojamas</p>}
+                                                </div >
+                                            </div >
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <label className='mb-2'>Slaptažodis</label>
+                                                    <input
+                                                        {...register("password",
+                                                            {
+                                                                required: true,
+                                                                minLength: 6
+                                                            })
+                                                        }
+                                                        type="password"
+                                                        className='form-control'
+                                                    />
+                                                    {errors?.password?.type === "required" && <p>Slaptažodis būtinas</p>}
+                                                    {errors?.password?.type === "minLength" && <p>Slaptažodis turi būti bent 6 raidžiu ilgumo</p>}
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex flex-row align-items-center mb-4">
+                                                <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                                                <div className="form-outline flex-fill mb-0">
+                                                    <label className='mb-2'>Pakartoti slaptažodį</label>
+                                                    <input
+                                                        {...register("password_repeat",
+                                                            {
+                                                                validate: value =>
+                                                                    value === password.current || "Slaptažodžiai nesutampa"
+                                                            })
+                                                        }
+                                                        type="password"
+                                                        className='form-control'
+                                                    />
+                                                    {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
+                                                </div>
+                                            </div>
+
+                                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                                <button type="submit" className="btn btn-primary btn-lg">Registruotis</button>
+                                            </div>
+                                        </form >
+
+                                    </div >
+                                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample image" />
+                                    </div>
+                                </div >
+                            </div >
+                        </div >
+                    </div >
+                </div >
+            </div >
+        </section >
+    );
+}
